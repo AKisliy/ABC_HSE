8b46bdc496f56fb4bb5b0711d6c98df132bd92ee
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-10-12 23:10:32 +0300

    Update workWithArray.asm

--- dz5/workWithArray.asm
+++ dz5/workWithArray.asm
@@ -1,4 +1,4 @@
-.global _fillArray
+.global _fillArray, _countSum
 .include "macrolib.inc"
 
 .text
@@ -13,4 +13,34 @@ _fillArray:
 	sub t1, t1,s2
 	bgtz t1, loop
 	pop(ra) # get returning adress from stack
+	ret
+
+_countSum:
+	push(ra)
+	mv t0, a1
+	mv t1, a2
+	li t2, 0
+	li t4, 0
+	countLoop:
+	lw t3, (t0)
+	bltz t3, negativeOverFlow
+	bgtz t3, positiveOverFlow
+	negativeOverFlow:
+		checkNegativeOverFlow(t3, t2)
+		j continue
+	positiveOverFlow:
+		checkPositiveOverFlow(t3, t2)
+	continue:
+	bgtz a0, endWithOverFlow
+	addi t4, t4,1
+	add t2, t2, t3
+	addi t0 t0 4
+	blt t4, t1, countLoop
+	j normalEnding
+	endWithOverFlow:
+	overFlowEnding(t4, t2)
+	normalEnding:
+	mv a0, t4
+	mv a1, t2
+	pop(ra)
 	ret
