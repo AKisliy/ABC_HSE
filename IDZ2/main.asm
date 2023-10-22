aba1366dbb18cba5ba0639b643bb3ec1acf0be41
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-10-22 11:07:59 +0300

    Update main.asm

--- IDZ2/main.asm
+++ IDZ2/main.asm
@@ -3,14 +3,19 @@
 .include "macrolib.inc"
 
 .data
-delta: .double 0.0001
+delta: .double 0.001
 
 .text
 main:
 	fld fs1, delta, t0
-	jal autoTests
+	userInputType # userInputType() -> int mode in a0(0 - noAuto, 1 - auto)
+	beqz a0, noAutoTests 
+	jal autoTests # starts auto tests
 	exit
-	userInput
-	arctan(fa0)
-	printArctanFrom(fa0)
+	noAutoTests:
+	userInput # userInput() -> double x in [-1;1](in fa0)
+	fmv.d fs0, fa0
+	arctan(fa0) # arctan(double x) -> double result = arctan(x) in fa0
+	fmv.d fa1, fa0
+	printArctan(fs0, fa1) # printArctanFrom(double x, double arg) -> print in console "arctan(x) = arg"
 	exit
