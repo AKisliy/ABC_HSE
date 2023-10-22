35ecdf61ffa45f3c1dc2886f72977d2baff84911
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-10-22 11:10:11 +0300

    Update autoTests.asm

--- IDZ2/autoTests.asm
+++ IDZ2/autoTests.asm
@@ -1,7 +1,9 @@
 .include "macrolib.inc"
 .global autoTests
 .data
+startingMessage: .asciz "Auto tests:\n"
 .align 3
+# test cases
 test1: .double -1
 test2: .double 1
 test3: .double 0
@@ -13,14 +15,16 @@ testEnd:
 .text
 autoTests:
 	push(ra)
-	la  s0, test1
+	printStr(startingMessage)
+	la  s0, test1 
 	la s1, testEnd
 	loop:
-	fld ft0, (s0)
-	arctan(ft0)
-	printArctanFrom(fa0)
-	addi s0, s0,8
-	beq s1, s0, end
+	fld fs0, (s0)
+	arctan(fs0) # find arctan of current test
+	fmv.d fa1, fa0
+	printArctan(fs0, fa1) # print arctan
+	addi s0, s0,8 # move to the next test case
+	beq s1, s0, end # if no more tests - end loop
 	j loop
 	end:
 	pop(ra)
