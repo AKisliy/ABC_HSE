b0c565f0be6afeadb1f85ebcebf0dd45d75272c5
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-11-07 09:50:24 +0300

    Create strnlen.s

---
+++ dz6/strnlen.s
@@ -0,0 +1,23 @@
+.globl strnlen
+
+.include "macro-syscalls.m"
+
+.text
+strnlen:
+    push(ra)
+    li      t0 0       
+loop:
+    lb      t1 (a0)   
+    beqz    t1 end
+    addi    t0 t0 1		
+    addi    a0 a0 1		
+    bge     t0 a1 fatal
+    b       loop
+end:
+    pop(ra)
+    mv      a0 t0
+    ret
+fatal:
+    pop(ra)
+    li      a0 -1
+    ret
