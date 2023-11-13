8c9e1c1531e04fb2dedc5843279bd0981809cbb5
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-11-13 11:31:02 +0300

    Create macro-syscalls.m

---
+++ IHW3/macro-syscalls.m
@@ -0,0 +1,139 @@
+.macro print_int (%x)
+	li a7, 1
+	mv a0, %x
+	ecall
+.end_macro
+
+.macro print_imm_int (%x)
+   li a7, 1
+   li a0, %x
+   ecall
+.end_macro
+
+.macro read_int_a0
+	li a7, 5
+	ecall
+.end_macro
+
+.macro read_int(%x)
+	push(a0)
+	li a7, 5
+	ecall
+	mv %x, a0
+	pop(a0)
+.end_macro
+
+.macro print_str (%x)
+.data
+str: .asciz %x
+.text
+	push (a0)
+	li a7, 4
+	la a0, str
+	ecall
+	pop(a0)
+.end_macro
+
+.macro print_str_from_label(%str)
+	la a0, %str
+	li a7,4
+	ecall
+.end_macro
+
+.macro print_char(%x)
+   	li a7, 11
+   	li a0, %x
+   	ecall
+.end_macro
+
+.macro newline
+   	print_char('\n')
+.end_macro
+
+.macro exit
+	li a7, 10
+	ecall
+.end_macro
+
+.macro push(%x)
+	addi sp, sp, -4
+	sw %x, (sp)
+.end_macro
+
+.macro pushbyte(%b)
+	push(t0)
+	li t0, %b
+	addi sp, sp, -1
+	sb t0, (sp)
+	pop(t0)
+.end_macro
+
+.macro pop(%x)
+	lw %x, (sp)
+	addi sp, sp, 4
+.end_macro
+
+.macro load_text
+	jal loadText
+.end_macro
+
+.macro str_get(%strbuf, %size)
+    la      a0 %strbuf
+    li      a1 %size
+    li      a7 8
+    ecall
+    push(s0)
+    push(s1)
+    push(s2)
+    li	s0 '\n'
+    la	s1	%strbuf
+next:
+    lb	s2  (s1)
+    beq s0	s2	replace
+    addi s1 s1 1
+    b	next
+replace:
+    sb	zero (s1)
+    pop(s2)
+    pop(s1)
+    pop(s0)
+.end_macro
+
+.eqv READ_ONLY	0	
+.eqv WRITE_ONLY	1	
+.eqv APPEND	9	
+.macro open(%file_name, %opt)
+    li   	a7 1024     	
+    la      a0 %file_name 
+    li   	a1 %opt 
+    ecall
+.end_macro
+
+.macro close(%file_descriptor)
+    li   a7, 57     
+    mv   a0, %file_descriptor  
+    ecall  
+.end_macro
+
+.macro allocate(%size)
+    li a7, 9
+    li a0, %size
+    ecall
+.end_macro
+
+.macro read_addr_reg(%file_descriptor, %reg, %size)
+    li   a7, 63
+    mv   a0, %file_descriptor    
+    mv   a1, %reg   
+    li   a2, %size 		
+    ecall             	
+.end_macro
+
+.macro print_change_of_string(%before, %after)
+.data
+arrow: .asciz " -> "
+.text
+	print_str_from_label(%before)
+	print_str_from_label(arrow)
+	print_str_from_label(%after)
+.end_macro
