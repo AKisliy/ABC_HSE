2db62d7afad49c48655f50a881f5f8f04bd44590
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-11-15 14:55:47 +0300

    Update load-text.s

--- IHW3/load-text.s
+++ IHW3/load-text.s
@@ -10,13 +10,28 @@
 .data
 er_name_mes:    .asciz "Incorrect file name\n"
 er_read_mes:    .asciz "Incorrect read operation\n"
+er_write_mes: .asciz "Incorrect write operation\n"
+resultVowels: .asciz "Number of vowels in your file: "
+resultConsonants: .asciz "Number of consonants in your file: "
 
 file_name:      .space	NAME_SIZE	
 strbuf:	.space TEXT_SIZE
+vowelsString: .space MAX_SIZE
+consonantsString: .space MAX_SIZE
+
 .text
 loadText:
     push(ra)
+    push(s0)
+    push(s1)
+    push(s2)
+    push(s3)
+    push(s4)
+    push(s5)
+    push(s6)
+    push(s11)
 inputFileLoop:
+    print_str ("ATTENTION!! Only files with a size <= 10 KB will be fully read!\n")
     print_str ("Input path to file for reading: ")
     str_get(file_name, NAME_SIZE)
     open(file_name, READ_ONLY)
@@ -52,34 +67,19 @@ end_loop:
     add t0 t0 s6	
     addi t0 t0 1	
     sb	zero (t0)
-
-    mv	a0 s3
-    li 	a7 4
-    ecall
-outputFileLoop: 
-    print_str ("Input path to file for writing: ")
-    str_get(file_name, NAME_SIZE) 
-    open(file_name, WRITE_ONLY)
-    li		s1 -1
-    beq		a0 s1 tryOutputAgain
-    j	continue2
-tryOutputAgain:
-	print_str_from_label(er_name_mes)
-    	j outputFileLoop
-continue2:
-    mv   	s0 a0   
-    li   a7, 64       		
-    mv   a0, s0 			
-    mv   a1, s3  			
-    mv   a2, s6    			
-    ecall
-
-    print_str ("There is bytes in the source file: ")
-    print_int(s6)
-    newline
     mv a0, s3
+    pop(s11)
+    pop(s6)
+    pop(s5)
+    pop(s4)
+    pop(s3)
+    pop(s2)
+    pop(s1)
+    pop(s0)
     pop(ra)
     ret
+    
 er_read:
-    print_str_from_label(er_read_mes)
-    exit
+print_str_from_label(er_read_mes)
+exit
+
