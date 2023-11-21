32cb64414371a2d817a63f5a6b94141a36691a71
AKisliy <124510676+AKisliy@users.noreply.github.com>
2023-11-21 09:59:53 +0300

    Update main.s

--- IHW3/main.s
+++ IHW3/main.s
@@ -5,16 +5,27 @@
 .text
 .globl main
 main:
+    userInputType # -> a0 = 0 - manual mode, a0 = 1 - autotests mode
+    beqz a0, manual
+    jal autoTests
+    exit
+    
+    manual:
     userChoice # userchoice() -> Y or N in a0
     mv s0, a0
+    
     newline
-    load_text # function to read text from file to heap, returns address of text's start in a0
+    getReadingPath # getReadingPath -> return correct file descriptor for reading in a0
+    loadTextFrom(a0)  # loadTextFrom(descriptor) - function to read text from file to heap, returns address of text's start in a0
+
     mv s3, a0
     vowelsAndConsonants(s3) # vowelsAndConsonants(address of string) -> num of vowels in a0, num of consonants in a1
     mv s10, a0
     mv s11, a1
     
-    write_result(s10,s11) # write_result(int numOfVowels, int numOfConsonants), writes results of count in file
+    getWritingPath # getWritingPath -> return correct file descriptor for writing in a0
+    mv s4, a0
+    write_result_to(s10,s11, s4) # write_result(int numOfVowels, int numOfConsonants, descriptor) - writes results of count in file
     
     li t0, 'Y' # print in console or not?
     bne t0, s0, noPrint
@@ -27,4 +38,5 @@ main:
     print_int(s11)
     noPrint:
     exit
+
 
